0f23f989e0d7c67834800399178deed91d11e3df
kevinsijo <kevinsijo@gmail.com>
2017-04-25 21:34:57 +0530

    Initializing admin.
    Signed-off-by: Kevin Sijo <kevinsijo@gmail.com>

---
+++ Angular/src/app/Login/Admin/admin-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pdf-admin-start',
+  templateUrl: './admin-start.component.html',
+  styleUrls: ['./admin-start.component.css']
+})
+export class AdminStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
