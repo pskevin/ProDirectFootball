1ead854506ca796905210623f13ed7c1cb87b340
Akshat Shetty <akshatshetty2908@gmail.com>
2017-04-26 02:12:36 +0530

    Added module for sms.

--- text.js
+++ text.js
@@ -1,29 +1,14 @@
-// Load the twilio module
 var twilio = require('twilio');
-
-// Create a new REST API client to make authenticated requests against the
-// twilio back end
 var client = new twilio.RestClient('AC7f3c18f7892caa2aaee44f474017e2c8','6ff1819b8dc09ebd338d35aafde0a76c');
-
-// Pass in parameters to the REST API using an object literal notation. The
-// REST client will handle authentication and response serialzation for you.
 client.sms.messages.create({
     to:'+919820717713',
     from:'+12053796263',
     body:'Sup Bitch!!!'
-}, function(error, message) {
-    // The HTTP request to Twilio will run asynchronously. This callback
-    // function will be called when a response is received from Twilio
-    // The "error" variable will contain error information, if any.
-    // If the request was successful, this value will be "falsy"
+},function(error, message) {
     if (!error) {
-        // The second argument to the callback will contain the information
-        // sent back by Twilio for the request. In this case, it is the
-        // information about the text messsage you just sent:
         console.log('Success! The SID for this SMS message is:');
         console.log(message.sid);
-
-        console.log('Message sent on:');
+      console.log('Message sent on:');
         console.log(message.dateCreated);
     } else {
         console.log('Oops! There was an error.'+err);
