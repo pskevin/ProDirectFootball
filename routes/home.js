38c92255441b9fa7bddc1d2e6220c8cf0a602b1e
Akshat Shetty <akshatshetty2908@gmail.com>
2017-04-26 14:59:28 +0530

    Updated home.js

--- routes/home.js
+++ routes/home.js
@@ -10,13 +10,7 @@ var port = 3000;
 app.use(morgan('dev'));
 var home = express.Router();
 home.use(bodyParser.json());
-home.route('/')
-/*
-.all(function(request,response,next){
-  response.writeHead(200,{'Content-Type' : 'text/html'});
-  next();
-})*/
-.all(function(request,response,next){
+home.all('/',function(request,response,next){
   console.log("in!");
   next();
 })
