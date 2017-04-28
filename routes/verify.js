a6e7fef956923f7b4d670cb2a6f560e2735071f4
Akshat Shetty <akshatshetty2908@gmail.com>
2017-04-29 00:41:57 +0530

    Updated verification functions.

--- routes/verify.js
+++ routes/verify.js
@@ -129,11 +129,16 @@ exports.trim_nulls = function (data) {
     var y;
     for (var x in data) {
         y = data[x];
-        console.log(y);
-        if (y === "null" || y === null || y === "" || typeof y === "undefined" || (y instanceof Object && Object.keys(y).length == 0)) {
+        console.log("Inside!:"+JSON.stringify(y).charAt(3));
+        if(JSON.stringify(y).charAt(2)=='$' && JSON.stringify(y).charAt(9)!='"')
+            continue;
+        if (y === "null" || y === null ||JSON.stringify(y)=='{"$in":[""]}'|| y === "" || typeof y === "undefined" || (y instanceof Object && Object.keys(y).length == 0)) {
+            console.log(data[x]);
             delete data[x];
+            console.log(data);
         }
-        if (y instanceof Object) y = trim_nulls(y);
+        if (y instanceof Object && JSON.stringify(y).charAt(2)!='$') y = trim_nulls(y);
+            console.log(data);
     }
     return data;
 };
