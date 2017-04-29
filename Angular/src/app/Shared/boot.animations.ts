fbd1a71ab28354d976ff55b16cbf91be45925ec8
Kevin Sijo <kevinsijo@gmail.com>
2017-04-29 15:44:27 +0530

    Setup of Pages.
    Signed-off-by: Kevin Sijo <kevinsijo@gmail.com>

---
+++ Angular/src/app/Shared/boot.animations.ts
@@ -0,0 +1,12 @@
+import { animate, style, transition, trigger } from "@angular/core";
+
+export const bootStateTrigger = trigger('bootState', [
+  transition(':enter', [
+    style({
+      opacity: 0
+    }),
+    animate('200ms', style({
+      opacity: 1
+    }))
+  ])
+]);
--- Angular/src/app/Terminal/terminal.component.ts
+++ Angular/src/app/Terminal/terminal.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from "../Shared/http.service";
 import { AuthService } from "../Shared/auth.service";
 import { EventService } from "../Shared/event.service";
 import { Boot } from "../Shared/boot.model";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'pdf-terminal',
@@ -10,17 +11,14 @@ import { Boot } from "../Shared/boot.model";
   styleUrls: ['./terminal.component.css']
 })
 export class TerminalComponent implements OnInit {
-  /*img1src: string= `../../assets/images/productImages/roundabout/sliders/141404.jpg`;
-   img2src: string= `../../assets/images/productImages/roundabout/sliders/141404(1).jpg`;
-   img3src: string= `../../assets/images/productImages/roundabout/sliders/141404(2).jpg`;
-   boottitle: string= `Puma evoSPEED 17.SL-S Kun City DF FG - Bluefish/Puma White/Quarry/Puma Black/Gold`;
-   bootcollection: string= `Collection Name`;
-   bootprice: string= `$200`;
-   bootdetails: string= `Featuring his club colours, this Derby Fever version of the PUMA evoSPEED 17.1 SL.S will be pulled on by Sergio Agüero when Manchester City face arch rivals Manchester United at the Etihad Stadium on Thursday 27th April 2017.<br><br>The PUMA evoSPEED 17.SL S is constructed with microfibre upper material for the upper, while a laser-cut SPEEDFRAME structure optimises the boot’s shape further, providing support during forward acceleration and quick turns.`;
-   */query: any;
+  //Declaring variables
+  query: any;
+  id: string;
+  brand: string;
   desc: string;
   name: string;
   collection: string;
+  imgthumb: string;
   img1src: string;
   img2src: string;
   img3src: string;
@@ -32,15 +30,18 @@ export class TerminalComponent implements OnInit {
   constructor (
     private http: HttpService,
     private auth: AuthService,
-    private event: EventService
+    private event: EventService,
+    private formBuilder: FormBuilder
   ) {
+    this.id = this.auth.getBootID();
     this.query = {
-      "_id": this.auth.getBootID ()
+      "_id": this.id
     };
     this.http.fetchBoot(this.query)
       .subscribe(
         (boot) => {
           console.log(boot);
+          this.brand = boot.brand;
           this.name = boot.bname;
           this.collection = boot.coll;
           this.price = boot.saleprice;
@@ -67,9 +68,18 @@ export class TerminalComponent implements OnInit {
   ngOnInit() {
   }
   
-  onSubmit(data: any){
+  onSubmit(data: any) {
     this.quantity = data.quantity;
-    this.auth.addToCart()
+    this.boot = new Boot(
+      this.id,
+      this.brand,
+      this.name,
+      this.collection,
+      this.imgthumb,
+      this.price,
+      this.status
+    );
+    this.auth.addToCart(this.boot,this.quantity);
     this.event.addToCart();
   }
 }
