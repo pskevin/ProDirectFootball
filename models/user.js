d5b6f666dd74634cf78795738c0e6fa4e9d3049e
Akshat Shetty <akshatshetty2908@gmail.com>
2017-05-01 02:33:51 +0530

    Updated user schema

--- models/user.js
+++ models/user.js
@@ -2,63 +2,57 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 var orderHistory = new Schema({
-  orderId:
-  {
-    type : String
-
-  },
-  bootId :
-  {
-    type: mongoose.Schema.Types.ObjectId,
-    ref : 'boots'
-  }
+    orderId :
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : 'order'
+        }
 });
-var dataSchema = new Schema(
-  {
-    username: String,
-    password: String,
-    Fname :
+var dataSchema = new Schema({
+        username: String,
+        password: String,
+        Fname :
+            {
+                type : String
+            },
+        Mname :
+            {
+                type : String
+            },
+        Lname :
+            {
+                type : String
+            },
+        email:{
+            type: String
+        },
+        mobno:
+            {
+                type:String
+            },
+        address:
+            {
+                type: String
+            },
+        admin:
+            {
+                type: Boolean,
+                default: false
+            },
+        verified :{
+            type :Boolean,
+            default : false
+        },
+        otp: Number,
+        logged:{
+            type :Boolean,
+            default: false
+        },
+        orders : [orderHistory]
+    },
     {
-      type : String
-    },
-    Mname :
-    {
-      type : String
-    },
-    Lname :
-    {
-      type : String
-    },
-    email:{
-      type: String
-    },
-    mobno:
-    {
-      type:String
-    },
-    address:
-    {
-      type: String
-    },
-    admin:
-    {
-      type: Boolean,
-      default: false
-    },
-    verified :{
-      type :Boolean,
-      default : false
-    },
-    otp: Number,
-    logged:{
-      type :Boolean,
-      default: false
-    },
-    orders : [orderHistory]
-  },
-  {
-    timestamps : true
-  }
+        timestamps : true
+    }
 );
 dataSchema.plugin(passportLocalMongoose);
 var Pro = mongoose.model('user',dataSchema);
