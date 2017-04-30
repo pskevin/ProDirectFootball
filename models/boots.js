1dcf8cb0d2c096b11c9c58ae27832ea0ddbe45db
Akshat Shetty <akshatshetty2908@gmail.com>
2017-05-01 02:33:19 +0530

    Updated boot schema

--- models/boots.js
+++ models/boots.js
@@ -2,67 +2,67 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 var CommentSchema = new Schema(
-  {
-    rating :
     {
-      type : Number,
-      min : 1,
-      max : 5
+        rating :
+            {
+                type : Number,
+                min : 1,
+                max : 5
+            },
+        remarks :
+            {
+                type : String
+            },
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        }
     },
-    remarks :
     {
-      type : String
-    },
-    postedBy: {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: 'user'
+        timestamps : true
     }
-  },
-  {
-    timestamps : true
-  }
 );
 var imageSchema = new Schema({
     name: String,
     data: String
 });
 var bootSchema = new Schema(
-  {
-    image :[imageSchema],
-    bname :
     {
-      type : String
+        image :[imageSchema],
+        bname :
+            {
+                type : String
+            },
+        brand:
+            {
+                type:String
+            },
+        coll:
+            {
+                type : String
+            },
+        description:
+            {
+                type:String
+            },
+        costprice :
+            {
+                type : Number
+            },
+        status:String,
+        saleprice :
+            {
+                type : Number
+            },
+        comments : {
+            type :[CommentSchema],
+            required : false,
+            unique : false
+        }
     },
-    brand:
     {
-        type:String
-    },
-    coll:
-    {
-     type : String
-    },
-    description:
-    {
-        type:String
-    },
-    costprice :
-    {
-        type : Number
-    },
-    status:String,
-    saleprice :
-    {
-      type : Number
-    },
-    comments : {
-        type :[CommentSchema],
-        required : false,
-        unique : false
+        timestamps : true
     }
-  },
-  {
-    timestamps : true
-  }
 );
 bootSchema.plugin(passportLocalMongoose);
 var Pro = mongoose.model('boot',bootSchema);
